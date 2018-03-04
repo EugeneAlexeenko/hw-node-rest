53d02d098ac52d0dd00b039412c92644284d5a32
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2018-03-04 23:19:48 +0300

    add axios

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
-console.log('hello pokemons');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const port = 3000;
+const pokeApi = `https://pokeapi.co/api/v2`;
+
+let pokemons;
+
+let dummyPokemons = [
+ {
+   id: 0,
+   name: 'pikachu'
+ },
+ {
+   id: 1,
+   name: 'cheburashka'
+ }
+];
+
+// get pokemons
+axios.get(`${pokeApi}/pokemon`)
+  .then(response => {
+    pokemons = response.data;
+    console.log(`pokemons received`);
+  })
+  .catch(e => {
+    console.log(e);
+  })
+
+// Router
+app.get('/', (req, res) => {
+  res.send(pokemons);
+});
+
+app.get(`${pokeApi}/pokemon/:id`, (req, res) => {
+  res.send(req.params);
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('error', err);
+  }
+
+  console.log(`App listening on port ${port}`);
+});
