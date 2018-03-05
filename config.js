0a9c7837c6ce58c1dd0d4fcb8d2a23790bbb3bcf
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2018-03-05 16:44:25 +0300

    add mongoDB

---
+++ config.js
@@ -0,0 +1,10 @@
+const dogApiUrl   = 'https://dog.ceo/api';
+const mongoDbPath = 'mongodb://localhost:32768/dogsDb';
+const port        = 3000;
+
+module.exports = {
+  dogApiUrl,
+  mongoDbPath,
+  port
+}
+
--- server.js
+++ server.js
@@ -1,46 +1,73 @@
-const express = require('express');
 const axios = require('axios');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const config = require('./config');
+
+const DOG_API_URL = config.dogApiUrl; 
+const MONGO_DB_PATH = config.mongoDbPath;
+const PORT = config.port;
+
 const app = express();
 
-const port = 3000;
-const pokeApi = `https://pokeapi.co/api/v2`;
+mongoose.connect(MONGO_DB_PATH); 
+const Schema = mongoose.Schema;
+const BreedDataSchema = new Schema({
+  breed: String,
+});
 
-let pokemons;
+const BreedData = mongoose.model('breedData', BreedDataSchema);
 
-let dummyPokemons = [
- {
-   id: 0,
-   name: 'pikachu'
- },
- {
-   id: 1,
-   name: 'cheburashka'
- }
-];
 
-// get pokemons
-axios.get(`${pokeApi}/pokemon`)
+axios.get(`${DOG_API_URL}/breeds/list`)
   .then(response => {
-    pokemons = response.data;
-    console.log(`pokemons received`);
+    console.log(`dog brees received succesfully!`);
+    let breeds = response.data.message;
+    fillDB(breeds);
   })
-  .catch(e => {
-    console.log(e);
-  })
+  .catch(err => console.log(err));
+
+function fillDB (breeds) {
+  breeds.forEach(breed => {
+    let data = new BreedData({
+      breed: breed
+    });  
+    data.save();
+  }); 
+}
 
 // Router
 app.get('/', (req, res) => {
-  res.send(pokemons);
+  res.send('hello from express');
 });
 
-app.get(`${pokeApi}/pokemon/:id`, (req, res) => {
-  res.send(req.params);
+app.get('/breeds', (req, res) => {
+  res.send(breeds.toString());
 });
 
-app.listen(port, (err) => {
+app.get('/breed/:id', (req, res) => {
+  console.log(req.params.id);
+  const breed = req.params.id;
+  let image;
+
+  axios.get(`${DOG_API_URL}/breed/${breed}/images/random`)
+    .then(response => {
+      console.log(`Breed ${breed} received!`);
+      console.log(response.data.message);
+      image = response.data.message;
+      res.send(image);
+    })
+    .catch(err => console.log(err));
+})
+
+// app.get(`${pokeApi}/pokemon/:id`, (req, res) => {
+//   res.send(req.params);
+// });
+
+app.listen(PORT, (err) => {
   if (err) {
     return console.log('error', err);
   }
 
-  console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${PORT}`);
 });
